81fda2b0f6bbcf8fb7b0bc205d5ae51e1af52d81
Daniel Raj <150522821+FroreVale@users.noreply.github.com>
2025-02-14 14:53:28 +0530

    Add Backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { Textarea } from "@/components/ui/textarea";
-
-
+import { useChat } from "@ai-sdk/react";
 export default function Home() {
+  const { messages, handleSubmit, input, handleInputChange } = useChat();
+
   return (
     <main className="fixed h-full w-full flex flex-col bg-muted ">
-      <div className="container h-full w-full flex flex-col py-8">
-        <div></div>
-        <form>
-          <Textarea 
-            className="w-full max-w-[600px] mx-auto text-lg"
+      <div className="container h-full w-full flex flex-col p-8">
+        <div className="flex-1 overflow-y-auto"></div>
+        <form onSubmit={handleSubmit} className="mt-auto relative">
+          <Textarea
+            className="w-full text-lg"
             placeholder="What's on your mind?"
+            value={input}
+            onChange={handleInputChange}
           />
         </form>
       </div>
